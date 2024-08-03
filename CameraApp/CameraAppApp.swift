8b381c1a2140e7676d6028d1e46abe56bdae2cca
富澤明大 <akihirowwzai04a@gmail.com>
2024-08-03 13:21:59 +0900

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by 富澤明大 on 2024/08/03.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
